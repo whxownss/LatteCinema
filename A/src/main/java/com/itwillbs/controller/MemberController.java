94a6d17b3d396f9ae51e40c8d8d0102881835bed
1 <1@DESKTOP-NS3G043>
2023-12-23 15:43:02 +0900

    test

---
+++ A/src/main/java/com/itwillbs/controller/MemberController.java
@@ -0,0 +1,34 @@
+package com.itwillbs.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MemberController extends HttpServlet {
+	RequestDispatcher dispatcher = null;
+	
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sPath = request.getServletPath();
+		
+		
+		if(sPath.equals("/main.me")) {
+			dispatcher = request.getRequestDispatcher("_a/main.jsp");
+		}
+	}
+	
+}
