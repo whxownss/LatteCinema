8efefb3004d15ac0935496d95e9b1b2ba5a3820f
y-e-o-p <duqdlduq2@naver.com>
2024-01-15 18:27:41 +0900

    성엽 01-15

---
+++ A/src/main/java/com/itwillbs/domain/StorePayDTO.java
@@ -0,0 +1,122 @@
+package com.itwillbs.domain;
+
+import java.sql.Timestamp;
+
+public class StorePayDTO {
+	private String memId;
+	private String payIdx;
+	private String paidAmount;
+	private String payMethod;
+	private String cardName;
+	private String cardNumber;
+	private String buyerTel;
+	private String success;
+	private String status;
+	private String buyerName;
+	private String merchantUid;
+	private String itemCnt;
+	private String name;
+	private String giftMsg;
+	private String giftTel;
+	
+	public String getMemId() {
+		return memId;
+	}
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+	public String getPayIdx() {
+		return payIdx;
+	}
+	public void setPayIdx(String payIdx) {
+		this.payIdx = payIdx;
+	}
+	public String getPaidAmount() {
+		return paidAmount;
+	}
+	public void setPaidAmount(String paidAmount) {
+		this.paidAmount = paidAmount;
+	}
+	public String getPayMethod() {
+		return payMethod;
+	}
+	public void setPayMethod(String payMethod) {
+		this.payMethod = payMethod;
+	}
+	public String getCardName() {
+		return cardName;
+	}
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getBuyerTel() {
+		return buyerTel;
+	}
+	public void setBuyerTel(String buyerTel) {
+		this.buyerTel = buyerTel;
+	}
+	public String getSuccess() {
+		return success;
+	}
+	public void setSuccess(String success) {
+		this.success = success;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getBuyerName() {
+		return buyerName;
+	}
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+	public String getMerchantUid() {
+		return merchantUid;
+	}
+	public void setMerchantUid(String merchantUid) {
+		this.merchantUid = merchantUid;
+	}
+	public String getItemCnt() {
+		return itemCnt;
+	}
+	public void setItemCnt(String itemCnt) {
+		this.itemCnt = itemCnt;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGiftMsg() {
+		return giftMsg;
+	}
+	public void setGiftMsg(String giftMsg) {
+		this.giftMsg = giftMsg;
+	}
+	public String getGiftTel() {
+		return giftTel;
+	}
+	public void setGiftTel(String giftTel) {
+		this.giftTel = giftTel;
+	}
+	
+	@Override
+	public String toString() {
+		return "StorePayDTO [memId=" + memId + ", payIdx=" + payIdx + ", paidAmount=" + paidAmount + ", payMethod="
+				+ payMethod + ", cardName=" + cardName + ", cardNumber=" + cardNumber + ", buyerTel=" + buyerTel
+				+ ", success=" + success + ", status=" + status + ", buyerName=" + buyerName + ", merchantUid="
+				+ merchantUid + ", itemCnt=" + itemCnt + ", name=" + name + ", giftMsg=" + giftMsg + ", giftTel="
+				+ giftTel + "]";
+	}
+	
+}
