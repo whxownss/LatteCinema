f65a9f0fbdadb8cc4b61f6f089d12b4afa0c841b
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-01-26 09:57:26 +0900

    Merge branch 'hjwBranch' of https://github.com/whxownss/movieTest.git

--- A/src/main/java/com/itwillbs/controller/MemberController.java
+++ A/src/main/java/com/itwillbs/controller/MemberController.java
@@ -236,17 +236,7 @@ public class MemberController extends HttpServlet {
 		if(sPath.equals("/simplelogin.me")) {
 			System.out.println("/simplelogin.me/ controller");
 			memberService = new MemberService();
-			//임시 추가 정지된 회원인지 판단하기.
-			// 회원 정지
-			MemberDTO memberDTO = memberService.userCheck(request);
-			if(memberDTO != null && memberDTO.getMemStatus().equals("1")) {
-				String memStopD = memberDTO.getMemStopD();
-				System.out.println("정지된날: "+memStopD);
-				request.setAttribute("memStopD", memStopD);
-				dispatcher = request.getRequestDispatcher("_member/msg2.jsp");
-				dispatcher.forward(request, response);
-				return;
-			}
+
 			//이메일 중복체크?
 			int result = memberService.checkEmail(request);
 			System.out.println(result);
